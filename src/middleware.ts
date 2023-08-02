cb852efd4b28df835183a1ebd48911d3c12aacb4
saydaleivs <saydaleiv@gmail.com>
2023-08-02 21:12:20 +0500

    unused import removed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { upload } from './config/gfsConfig'
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
@@ -8,12 +7,6 @@ export function middleware(request: NextRequest) {
   const isPublicPath = path === '/signin' || path === '/signup'
 
   const token = request.cookies.get('token')?.value || ''
-
-  // if (path === '/api/uploads') {
-  //   console.log('/api/uploads')
-
-  // upload.single('file')
-  // }
 
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
